ba4ef7c759c3d05e22f76ceef8a35c653d049324
Anjan Biswas <anjan@Anjans-MacBook-Pro-2.local>
2018-05-31 11:14:01 -0700

    Moved to Extension

--- CustomSearchBarSwift/ViewController.swift
+++ CustomSearchBarSwift/ViewController.swift
@@ -25,34 +25,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         setupNavBar()
     }
     
-    func setupNavBar(){
-        self.navigationController?.navigationBar.setBackgroundImage(nil, for: .default)
-        self.navigationController?.navigationBar.shadowImage = nil
-        self.navigationController?.navigationBar.isTranslucent = false
-        self.navigationController?.navigationBar.tintColor = nil
-        
-        searchBar.frame = CGRect(x: 0, y: 0, width: 340, height: 34)
-        let centeredParagraphStyle = NSMutableParagraphStyle()
-        centeredParagraphStyle.alignment = .left
-        let attributes = [
-            NSAttributedStringKey.foregroundColor: UIColor(hex: "#37474F"),
-            NSAttributedStringKey.paragraphStyle: centeredParagraphStyle
-        ]
-        self.searchBar.layer.cornerRadius = 15
-        self.searchBar.backgroundColor = UIColor(hex: "#ECEFF1")
-        let centerText = NSMutableAttributedString(string: "Search", attributes: attributes)
-        self.searchBar.attributedPlaceholder = centerText
-    
-        let imageView = UIImageView()
-        let image = UIImage(named: "searchfield")
-        imageView.image = image
-        imageView.frame = CGRect(x: 0, y: 0, width: 25, height: 15)
-        imageView.contentMode = .scaleAspectFit
-        searchBar.leftViewMode = .always
-        searchBar.leftView = imageView
-        self.navigationItem.titleView = searchBar
-    }
-
     func textFieldDidBeginEditing(_ textField: UITextField) {
         self.searchBar.becomeFirstResponder()
     }
